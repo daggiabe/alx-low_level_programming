c2ef22d4c05af629e62edfb43c2f898f69429fbe
daggiabe <daggiabe@gmail.com>
2022-12-09 06:51:57 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,10 @@
-/*
- *  * File: 2-print_alphabet.c
- *   * Auth: Brennan D Baraban
- *    */
-
 #include <stdio.h>
 
 /**
  *  * main - Prints the alphabet in lowercase.
- *   *
- *    * Return: Always 0.
- *     */
+ *
+ *Return: Always 0.
+ */ 
 int main(void)
 {
 		char letter;
