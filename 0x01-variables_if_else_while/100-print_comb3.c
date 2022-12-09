43b15c359d6ff6525989f7fe7328796a9fdc41af
daggiabe <daggiabe@gmail.com>
2022-12-09 08:24:10 -0800

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,19 +2,17 @@
  * File: 0-positive_or_negative.c
  * Auth: Brennan D Baraban
  */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
- /**
-  * main - Prints a random number and states whether
-  *        it is positive, negative, or zero.
-  *
-  * Return: Always 0.
-  */
- int main(void)
+/**
+ * main - Prints a random number and states whether
+ * it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ */
 {
+int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
