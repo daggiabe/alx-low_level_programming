6791efb625c49add64bcf870b60bd551bfd0fe0d
daggiabe <daggiabe@gmail.com>
2022-12-12 03:53:54 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 
-
 /**
  * main - Entry point
  *
@@ -9,6 +8,6 @@
 
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return(1);
 }
