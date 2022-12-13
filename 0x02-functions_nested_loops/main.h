74c544fa1203aab9c9343c72c4ef782647a84e6c
daggiabe <daggiabe@gmail.com>
2022-12-13 08:31:00 -0800

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,6 +12,8 @@ void jack_bauer(void)
 void times_table(void)
 int add(int, int)
 void print_to_98(int n);
+void num(int r, int n);
+void print_times_table(int n);
 int _putchar(char c)
 
 #endif
